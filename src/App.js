c254bb4570ffcbb07e06ada74fb6a11072c7971c
iginot <igor.truchlik@gmail.com>
2020-05-14 13:39:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Map, Marker, Popup, TileLayer} from "react-leaflet";
+import { Icon } from "leaflet";
 import './App.css';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map center = {[59.33, 18.06]} zoom={14} onclick={function(e) {
+      alert(e.latlng);
+    }}>
+      <TileLayer
+       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+       attribution='&copy; <a href="https://www.osm.org/copyright">OpenStreetMap</a> contributors'
+    />
+      
+      <Marker
+        key={[1]}
+        position={[59.33, 18.06]}
+      >
+        <Popup>
+          This is that text after the click..
+        </Popup>
+      </Marker>
+    
+    </Map>
+
   );
 }
 
-export default App;
